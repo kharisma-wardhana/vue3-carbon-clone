508068482085545db09b1f5fe293f6c2fe11af3a
Kharisma wardhana <nanda.kharisma29@gmail.com>
2020-10-10 15:14:38 +0700

    implement page list Code

---
+++ src/views/Code.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="px-8 pt-2">
+    <ListCode />
+  </section>
+</template>
+
+<script>
+import ListCode from "../components/section/ListCode";
+export default {
+  components: {
+    ListCode
+  }
+};
+</script>
